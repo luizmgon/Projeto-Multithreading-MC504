97a7e7d2aa5923342a5dced99c504f14205cc452
Cícero Libardi <c168810@dac.unicamp.br>
2023-05-14 00:55:06 -0300

    adicionando novos detalhes ao código

--- river_crossing_problem.c
+++ river_crossing_problem.c
@@ -5,45 +5,68 @@
 #include <unistd.h>
 #include <math.h>
 
+#define N_HACKERS 10
+#define N_SERFS 10
 
-#define N_hackers 20
-#define N_serfs 20
-
+// Variáveis globais do problema.
 pthread_barrier_t barrier;
-pthread_barrierattr_t attr;
 sem_t mutex;
 sem_t serf_queue;
 sem_t hacker_queue;
 int hackers = 0;
 int serfs = 0;
 
-void board(char category){
-    if (category == 's'){
+// Ação de embarque dos personagens no barco.
+void board(char category)
+{
+    if (category == 's')
+    {
         printf("-----------Embarcou um microsofter--------------\n\n");
     }
-    else{
+    else
+    {
         printf("-----------Embarcou um hacker-----------------\n\n");
     }
 }
 
-void rowBoat(){
+// Ação de partida do barco.
+void rowBoat()
+{
     printf("O barco partiu\n\n");
+    usleep(1000000);
 }
 
-void *thread_serfs(){
+// Chegada de um novo Microsofter na fila.
+void newSerfArrived()
+{
+    serfs += 1;
+    printf("Chegou um microsofter\n\n");
+}
+
+// Chegada de um novo Hacker na fila.
+void newHackerArrived()
+{
+    hackers += 1;
+    printf("Chegou um hacker\n\n");
+}
+
+// Thread de comportamento dos Microsofters.
+void *thread_serfs()
+{
     int is_captain = 0;
     sem_wait(&mutex);
 
-    serfs += 1;
-    printf("Chegou um microsofter\n\n");
+    newSerfArrived();
 
-    if (serfs == 4){
+    if (serfs == 4)
+    {
         for (int i = 0; i < 4; i ++)
             sem_post(&serf_queue);
         serfs = 0;
         is_captain = 1;
     }
-    else if ((serfs == 2) && (hackers >= 2)){
+    else if ((serfs == 2) && (hackers >= 2))
+    {
         for (int i = 0; i < 2; i ++){
             sem_post(&serf_queue);
             sem_post(&hacker_queue);
@@ -52,7 +75,8 @@ void *thread_serfs(){
         serfs = 0;
         is_captain = 1;
     }
-    else{
+    else
+    {
         sem_post(&mutex);
     }
 
@@ -61,26 +85,30 @@ void *thread_serfs(){
     board('s');
     pthread_barrier_wait(&barrier);
 
-    if (is_captain){
+    if (is_captain)
+    {
         rowBoat();
         sem_post(&mutex);
     }
 }
 
-void *thread_hackers(){
+// Thread de comportamento dos Hackers.
+void *thread_hackers()
+{
     int is_captain = 0;
     sem_wait(&mutex);
 
-    hackers += 1;
-    printf("Chegou um hacker\n\n");
+    newHackerArrived();
 
-    if (hackers == 4){
+    if (hackers == 4)
+    {
         for (int i = 0; i < 4; i ++)
             sem_post(&hacker_queue);
         hackers = 0;
         is_captain = 1;
     }
-    else if ((hackers == 2) && (serfs >= 2)){
+    else if ((hackers == 2) && (serfs >= 2))
+    {
         for (int i = 0; i < 2; i ++){
             sem_post(&hacker_queue);
             sem_post(&serf_queue);
@@ -89,7 +117,8 @@ void *thread_hackers(){
         hackers = 0;
         is_captain = 1;
     }
-    else{
+    else
+    {
         sem_post(&mutex);
     }
 
@@ -98,36 +127,57 @@ void *thread_hackers(){
     board('h');
     pthread_barrier_wait(&barrier);
 
-    if (is_captain){
+    if (is_captain)
+    {
         rowBoat();
         sem_post(&mutex);
     }
 }
 
+int main()
+{
 
-int main(){
-    // Instanciando o problema inicial
-    
-    pthread_barrier_init(&barrier, &attr, 4);
-
+    // Inicialização de semáforos e barreiras.
+    pthread_barrier_init(&barrier, NULL, 4);
     sem_init(&mutex, 0, 1);
     sem_init(&hacker_queue, 0, 0);
     sem_init(&serf_queue, 0, 0);
 
-    pthread_t thr_hackers[N_hackers];
-    pthread_t thr_serfs[N_serfs];
+    // Inicialização das Threads.
+    pthread_t thr_hackers[N_HACKERS];
+    pthread_t thr_serfs[N_SERFS];
 
-    int n_hackers_left = N_hackers;
-    int n_serfs_left = N_serfs;
+    int n_hackers_left = N_HACKERS;
+    int n_serfs_left = N_SERFS;
 
-    while (!(((n_hackers_left % 4) == 0 && (n_serfs_left % 4) == 0) || (((n_hackers_left % 2 == 0) && (n_serfs_left % 2 == 0)) && ((n_hackers_left % 4 != 0) && (n_serfs_left % 4 != 0))))){
-        if (n_hackers_left % 2 != 0){
+    /*Correção do número de Hackers e Microsofters de entrada para evitar deadlock ao final da execução.
+
+    Explicação:
+
+    Existem duas condições que devem ser satisfeitas para que todos os Hackers e Microsofters
+    completem a viagem:
+    - Eles devem ser AMBOS múltiplos de 4.
+    ou
+    - Eles devem ser AMBOS pares mas não múltiplos de 4.
+
+    Caso essas condições não sejam satisfeitas, sobrarão, ao final da execução, indivíduos que
+    não formaram grupos para travessia e que ficariam eternamente aguardando.
+    Por isso é feita essa correção de não colocá-los na fila e posteriormente é indicado
+    quem não conseguiu atravessar.*/
+    if (!(
+            ((n_hackers_left % 4) == 0 && (n_serfs_left % 4) == 0) ||
+            (((n_hackers_left % 2 == 0) && (n_serfs_left % 2 == 0)) && ((n_hackers_left % 4 != 0) && (n_serfs_left % 4 != 0)))))
+    {
+        if (n_hackers_left % 2 != 0)
+        {
             n_hackers_left--;
         }
-        if (n_serfs_left % 2 != 0){
+        if (n_serfs_left % 2 != 0)
+        {
             n_serfs_left--;
         }
-        if (!(((n_hackers_left % 2 == 0) && (n_serfs_left % 2 == 0)) && ((n_hackers_left % 4 != 0) && (n_serfs_left % 4 != 0)))){
+        if (!(((n_hackers_left % 2 == 0) && (n_serfs_left % 2 == 0)) && ((n_hackers_left % 4 != 0) && (n_serfs_left % 4 != 0))))
+        {
             if (n_hackers_left > n_serfs_left)
                 n_hackers_left -= 2;
             else
@@ -135,35 +185,45 @@ int main(){
         }
     }
 
-    int maximo;
-    maximo = n_hackers_left > n_serfs_left ? n_hackers_left : n_serfs_left;
-    
-    for (int i = 0; i < maximo; i++){
-        if (i < n_hackers_left){
-            // sleep(1);
+    int maximo = n_hackers_left > n_serfs_left ? n_hackers_left : n_serfs_left;
+
+    // Criação das Threads.
+    for (int i = 0; i < maximo; i++)
+    {
+        if (i < n_hackers_left)
+        {
+            usleep(500000);
             pthread_create(&thr_hackers[i], NULL, thread_hackers, NULL);
         }
-        if (i < n_serfs_left){
-            // sleep(1);
+        if (i < n_serfs_left)
+        {
+            usleep(500000);
             pthread_create(&thr_serfs[i], NULL, thread_serfs, NULL);
         }
     }
 
-    for (int i = 0; i < maximo; i++){
-        if (i < n_hackers_left){
+    // Espera de finalização das Threads.
+    for (int i = 0; i < maximo; i++)
+    {
+        if (i < n_hackers_left)
+        {
             pthread_join(thr_hackers[i], NULL);
         }
-        if (i < n_serfs_left){
+        if (i < n_serfs_left)
+        {
             pthread_join(thr_serfs[i], NULL);
         }
     }
 
-    for (int i = 0; i < (N_hackers - n_hackers_left); i++){
+    // Indicação dos indíviduos que foram removidos por não formarem grupos necessários para a travessia.
+    for (int i = 0; i < (N_HACKERS - n_hackers_left); i++)
+    {
         sem_post(&hacker_queue);
         printf("Um hacker não conseguiu atravessar\n\n");
     }
 
-    for (int i = 0; i < (N_serfs - n_serfs_left); i++){
+    for (int i = 0; i < (N_SERFS - n_serfs_left); i++)
+    {
         sem_post(&serf_queue);
         printf("Um microsofter não conseguiu atravessar\n\n");
     }
