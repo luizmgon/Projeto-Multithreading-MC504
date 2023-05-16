3075fa06f26a82256e3ef1119f10439961806b1e
Isabella Fagioli <isabella.fagioli@gmail.com>
2023-05-16 18:56:25 -0300

    inicio da função do barquinho remando

--- river_image.c
+++ river_image.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define ESPACOS 25
-#define RIO 75
+#define RIO 100
 
 
 /* O desenho deve ter o seguinte formato:
@@ -24,12 +24,12 @@ Barquinho em movimento
 ======================|
     Microsofter       |
                       |                                       
-                      |    \                                /
-                      |     \                              /
-                      |__    \        M   H   M   H       /
-                         |    \       O   O   O   O      /
-                       __|     \     -|- -|- -|- -|-    /
-                      |	        \____/ \_/ \_/ \_/ \___/
+                      |\                                /
+                      | \                              /
+                      |__\        M   H   M   H       /
+                         |\       O   O   O   O      /
+                       __| \     -|- -|- -|- -|-    /
+                      |	    \____/ \_/ \_/ \_/ \___/
       Hackers         |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                       |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                       |
@@ -119,6 +119,220 @@ void estado_atual_chegada(int nhackers, int nserfs){
     }
     printf("/\n");
     for(int i=0;i<2*ESPACOS+2;i++){
+        printf(" ");
+    }
+    printf("/\n");
+    for(int i=0;i<ESPACOS;i++){
+        printf(" ");
+    }
+    printf(" ____");
+    for(int i=0;i<ESPACOS-4;i++){
+        printf(" ");
+    }
+    printf("/\n");
+    for(int i=0;i<ESPACOS;i++){
+        printf(" ");
+    }
+    printf("|");
+    for(int i=0;i<ESPACOS-1;i++){
+        printf("_");
+    }
+    printf("/\n");
+
+    //título
+    int espac_hackers = ((ESPACOS-7)/2);
+    for(int i=0;i<espac_hackers;i++){
+        printf(" ");
+    }
+    printf("Hackers"); 
+    for(int i=0;i<espac_hackers;i++){
+        printf(" ");
+    }
+    printf("|");
+    //rio
+    for(int i=0;i<RIO;i++){
+        printf("~");
+    }
+    printf("\n"); 
+    //cabeças
+    espac_cabeca = ESPACOS;
+    for(int i;i<nhackers;i++){
+        printf("   O"); 
+        espac_cabeca = espac_cabeca - 4;
+    }
+    for(int i=0;i<espac_cabeca;i++){
+        printf(" ");
+    }
+    printf("|");
+    //rio
+    for(int i=0;i<RIO;i++){
+        printf("~");
+    }
+    printf("\n"); 
+
+    //corpinho
+    espac_corpinho = ESPACOS;
+    for(int i;i<nhackers;i++){
+        if(i==0){
+            printf("  -|-"); 
+            espac_corpinho = espac_corpinho - 5;
+        }
+        else{
+            printf(" -|-"); 
+            espac_corpinho = espac_corpinho - 4;
+        }
+    }
+    for(int i=0;i<espac_corpinho;i++){
+        printf(" ");
+    }
+    printf("|");
+
+    //rio
+    for(int i=0;i<RIO;i++){
+        printf("~");
+    }
+    printf("\n"); 
+    //pernas
+    espac_pernas = ESPACOS;
+    for(int i;i<nhackers;i++){
+        if(i==0){
+            printf("  / \\"); 
+            espac_pernas = espac_pernas - 5;
+        }
+        else{
+            printf(" / \\"); 
+            espac_pernas = espac_pernas - 4;
+        }
+    }
+    for(int i=0;i<espac_pernas;i++){
+        printf(" ");
+    }
+    printf("|");
+    //rio
+    for(int i=0;i<RIO;i++){
+        printf("~");
+    }
+    printf("\n"); 
+    //base
+    for(int i = 0;i<ESPACOS;i++){
+        printf("="); 
+    }
+    printf("|\n");
+}
+
+void remando(int nhackers, int nserfs, int hackers_barco, int serfs_barco, int estagio){
+    //topo
+    for(int i = 0;i<ESPACOS;i++){
+        printf("="); 
+    }
+    printf("|\n");
+
+    //título
+    int espac_serf = ((ESPACOS-11)/2);
+    for(int i=0;i<espac_serf;i++){
+        printf(" ");
+    }
+    printf("Microsofter"); 
+    for(int i=0;i<espac_serf;i++){
+        printf(" ");
+    }
+    printf("|\n");
+
+    //cabeças
+    int espac_cabeca = ESPACOS;
+    for(int i;i<nserfs;i++){
+        printf("   O"); 
+        espac_cabeca = espac_cabeca - 4;
+    }
+    for(int i=0;i<espac_cabeca;i++){
+        printf(" ");
+    }
+    printf("|\n");
+
+    //corpinho
+    int espac_corpinho = ESPACOS;
+    for(int i;i<nserfs;i++){
+        if(i==0){
+            printf("  -|-"); 
+            espac_corpinho = espac_corpinho - 5;
+        }
+        else{
+            printf(" -|-"); 
+            espac_corpinho = espac_corpinho - 4;
+        }
+        
+    }
+    for(int i=0;i<espac_corpinho;i++){
+        printf(" ");
+    }
+    printf("|");
+    for(int i=0;i<estagio*ESPACOS;i++){
+        printf(" ");
+    }
+    printf("\\");
+    for(int i=0;i<ESPACOS+4;i++){
+        printf(" ");
+    }
+    printf("/\n");
+    //pernas
+    int espac_pernas = ESPACOS;
+    for(int i;i<nserfs;i++){
+        if(i==0){
+            printf("  / \\"); 
+            espac_pernas = espac_pernas - 5;
+        }
+        else{
+            printf(" / \\"); 
+            espac_pernas = espac_pernas - 4;
+        }
+    }
+    for(int i=0;i<espac_pernas;i++){
+        printf(" ");
+    }
+    printf("| ");
+    for(int i=0;i<estagio*ESPACOS;i++){
+        printf(" ");
+    }
+    printf("\\");
+    for(int i=0;i<ESPACOS+2;i++){
+        printf(" ");
+    }
+    printf("/\n");
+
+    //caminho pro barco
+    for(int i=0;i<ESPACOS;i++){
+        printf(" ");
+    }
+    printf("|__");
+    for(int i=0;i<estagio*ESPACOS;i++){
+        printf(" ");
+    }
+    printf("\\");
+    for(int i=0;i<ESPACOS;i++){
+        printf(" ");
+    }
+    printf("/\n");
+    for(int i=0;i<ESPACOS+2;i++){
+        printf(" ");
+    }
+    printf(" |");
+    for(int i=0;i<estagio*ESPACOS;i++){
+        printf(" ");
+    }
+    printf("\\");
+    for(int i=0;i<ESPACOS-2;i++){
+        printf(" ");
+    }
+    printf("/\n");
+    for(int i=0;i<ESPACOS;i++){
+        printf(" ");
+    }
+    printf(" __| ");
+    for(int i=0;i<estagio*ESPACOS;i++){
+        printf(" ");
+    }
+    printf("\\");
+    for(int i=0;i<ESPACOS-4;i++){
         printf(" ");
     }
     printf("/\n");
